fddc185e53f5193b1cabe13225a8fe13fd2b81d5
Diptifirke <diptifirke03@gmail.com>
2026-08-25 20:46:01 +0530

    Added Binary Tree ZigZag Level Order Traversal

---
+++ Graphs/BinaryTreeZigZagLevelOrderTraversal.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            LinkedList<Integer> currentLevel = new LinkedList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+
+                if (leftToRight) {
+                    currentLevel.addLast(node.val);
+                } else {
+                    currentLevel.addFirst(node.val);
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            result.add(currentLevel);
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+}
